47a06fa663ef2366876d06b675ce10e92a54a2ce
ankitkathaitproton <ankitkathait@proton.me>
2026-06-04 18:14:40 +0530

    init

---
+++ src/main/java/com/example/multi_tenant_notifcation_ankit_kathait/MultiTenantNotifcationAnkitKathaitApplication.java
@@ -0,0 +1,13 @@
+package com.example.multi_tenant_notifcation_ankit_kathait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantNotifcationAnkitKathaitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenantNotifcationAnkitKathaitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multi_tenant_notifcation_ankit_kathait/MultiTenantNotifcationAnkitKathaitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multi_tenant_notifcation_ankit_kathait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenantNotifcationAnkitKathaitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
